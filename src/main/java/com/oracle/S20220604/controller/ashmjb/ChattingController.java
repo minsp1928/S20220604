a02a2549b95981300fba2bdcccaec8d7af491cd8
Emmakang123 <101373946+Emmakang123@users.noreply.github.com>
2022-06-03 13:51:29 +0900

    minjoo1
    폼수정, 핸들러생성, 메시지 서비스, 클래스와 인터페이스 생성,
    선생님과 같은 jsp로 화면생성

---
+++ src/main/java/com/oracle/S20220604/controller/ashmjb/ChattingController.java
@@ -0,0 +1,23 @@
+package com.oracle.S20220604.controller.ashmjb;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+@RestController
+public class ChattingController {
+	@RequestMapping("/chat")
+	public ModelAndView chat() {
+		System.out.println("ChattingController chat start");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/chatAshmjb/chatRoomMain");
+		return mv;
+	}
+	@RequestMapping("/test")
+	public ModelAndView test() {
+		System.out.println("ChattingController chat start");
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/chatAshmjb/text");
+		return mv;
+	}
+}
---
+++ src/main/java/com/oracle/S20220604/service/ashmjb/MessageServiceImpl.java
@@ -0,0 +1,16 @@
+package com.oracle.S20220604.service.ashmjb;
+
+import com.oracle.S20220604.model.Message;
+
+public class MessageServiceImpl implements MessageService {
+
+	@Override
+	public int insert(Message message) {
+		int result = 0;
+		System.out.println("MessageServiceImpl insert Start...");
+		
+		
+		return 0;
+	}
+
+}
